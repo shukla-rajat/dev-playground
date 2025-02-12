3c962126f73dd3c9b750ed198642b1f87f210b0a
Rajat Shukla <rajatshukla799@gmail.com>
2025-02-12 15:59:38 +0100

    encapsulated sa=tate and state update logic into custom Provider component and custom context hook

--- src/currency-context.tsx
+++ src/currency-context.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
 
+const CURRENCIES = {
+    Euro: {
+      symbol: '€',
+      label: 'Euro'
+    },
+    Usd: {
+      symbol: '$',
+      label: 'Dollor'
+    },
+  };
+  
 const CurrencyContext = React.createContext(null);
-const useCurrency = () => React.useContext(CurrencyContext);
+
 const CurrencyProvider = ({ value, children }) => {
-    return (<CurrencyContext.Provider value={value}>
+    const [currency, setCurrency] = React.useState(CURRENCIES.Usd);
+    return (<CurrencyContext.Provider value={[currency, setCurrency]}>
         {children}
     </CurrencyContext.Provider>
     );
 }
 
-export { CurrencyProvider, useCurrency };
+const useCurrency = () => {
+    const[currency, setCurrency] = React.useContext(CurrencyContext);
+    const handleCurrency = (value) => {
+        setCurrency(value);
+    }
+    return { value:currency, onChange: handleCurrency }
+}
+
+export { CurrencyProvider, useCurrency, CURRENCIES };
