27cfc1971819cdc3a5a3b9eb5eda71e4338db63f
Rajat Shukla <rajatshukla799@gmail.com>
2025-02-12 15:22:20 +0100

    implemented custom hook for context provider and consumer

--- src/currency-context.tsx
+++ src/currency-context.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
 const CurrencyContext = React.createContext(null);
+const useCurrency = () => React.useContext(CurrencyContext);
+const CurrencyProvider = ({ value, children }) => {
+    return (<CurrencyContext.Provider value={value}>
+        {children}
+    </CurrencyContext.Provider>
+    );
+}
 
-export { CurrencyContext };
+export { CurrencyProvider, useCurrency };
